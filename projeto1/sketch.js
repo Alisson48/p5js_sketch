a9dce9daf1b3b29da1a1031e0a7c9a51a107d4d3
Alisson48 <138634226+Alisson48@users.noreply.github.com>
2023-09-19 22:20:02 -0300

    Add files via upload

---
+++ projeto1/sketch.js
@@ -0,0 +1,47 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function setup() {
+
+createCanvas(600, 600);
+
+}
+
+function draw() {
+
+background("white");
+
+stroke("blue") // alterando a cor da borda do desenho
+
+fill("red") // alterando a cor do preenchimento do desenho
+
+rect(100, 250, 100, 150)
+
+}
+
+function draw() {
+  background(220);
+}
+
+function setup() {
+
+createCanvas(600, 600);
+
+background("white");
+
+}
+
+function draw() {
+
+stroke("blue");
+
+fill("red");
+
+if(mouseIsPressed){
+
+rect(mouseX, mouseY, 20, 35);
+
+}
+
+}
